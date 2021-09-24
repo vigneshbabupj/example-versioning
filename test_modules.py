90f135441929ff04824cd4ab3f566f2107501ebd
vigneshbabupj <vigneshbabupj@gmail.com>
2021-09-24 13:44:41 +0530

    Corrected filename in test.py

--- test_modules.py
+++ test_modules.py
@@ -11,16 +11,16 @@ def test_model_file():
     assert not os.path.exists(file), "model.h5 is getting uploaded"
     
 def test_model_acc():
-    metrics = pd.read_csv('metrics.csv')
+    metrics = pd.read_csv('Metrics.csv')
     acc = metrics.iloc[-1]['accuracy']
     assert acc > 0.7, "Overall acc is < 70%"
 
 def test_cat_acc():
-    metrics = pd.read_csv('metrics.csv')
+    metrics = pd.read_csv('Metrics.csv')
     acc = metrics.iloc[-1]['cat_acc']
     assert acc > 0.7, "Cat class acc is < 70%"
         
 def test_dog_acc():
-    metrics = pd.read_csv('metrics.csv')
+    metrics = pd.read_csv('Metrics.csv')
     acc = metrics.iloc[-1]['dog_acc']
     assert acc > 0.7, "Dog class acc is < 70%"
